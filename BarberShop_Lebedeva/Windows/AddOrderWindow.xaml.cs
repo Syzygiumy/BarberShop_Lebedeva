9d5cf761f600d8a6d4e47f0f61c3173f11adbace
IMac2 <IMac2@DESKTOP-242P176>
2022-03-01 12:43:57 +0300

    111

--- BarberShop_Lebedeva/Windows/AddOrderWindow.xaml.cs
+++ BarberShop_Lebedeva/Windows/AddOrderWindow.xaml.cs
@@ -31,9 +31,14 @@ namespace BarberShop_Lebedeva.Windows
             cmb_TitleName.DisplayMemberPath = "Title";
             cmb_TitleName.SelectedIndex = 0;
 
-            //cmb_Status.ItemsSource = ClassesHelper.AppData.context.Order.ToList();
-            //cmb_Status.DisplayMemberPath = "IsCompleted";
-            //cmb_Status.SelectedIndex = 0;
+            cmb_LNameClient.ItemsSource = ClassesHelper.AppData.context.Client.ToList();
+            cmb_LNameClient.DisplayMemberPath = "LName";
+            cmb_LNameClient.SelectedIndex = 0;
+
+            cmb_LNamePersonal.ItemsSource = ClassesHelper.AppData.context.Emploee.ToList();
+            cmb_LNamePersonal.DisplayMemberPath = "LName";
+            cmb_LNamePersonal.SelectedIndex = 0;
+
         }
 
         //public AddOrderWindow(EF.Order order)
@@ -59,36 +64,37 @@ namespace BarberShop_Lebedeva.Windows
         private void btn_AddOrder_Click(object sender, RoutedEventArgs e)
         {
 
-            //Проверка на пустоту
-            if (string.IsNullOrWhiteSpace(txt_LNameClient.Text))
-            {
-                MessageBox.Show("Поле 'Имя' не должно быть пустым", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
+            ////Проверка на пустоту
+            //if (string.IsNullOrWhiteSpace(txt_LNameClient.Text))
+            //{
+            //    MessageBox.Show("Поле 'Имя' не должно быть пустым", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            //    return;
+            //}
 
-            if (string.IsNullOrWhiteSpace(txt_LNamePersonal.Text))
-            {
-                MessageBox.Show("Поле 'Фамилия' не должно быть пустым", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
+            //if (string.IsNullOrWhiteSpace(txt_LNamePersonal.Text))
+            //{
+            //    MessageBox.Show("Поле 'Фамилия' не должно быть пустым", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            //    return;
+            //}
 
             var resClick = MessageBox.Show("Вы уверены?", "Вопрос", MessageBoxButton.YesNo, MessageBoxImage.Question);
 
-            if(resClick == MessageBoxResult.Yes)
+            if (resClick == MessageBoxResult.Yes)
             {
-                var Client = ClassesHelper.AppData.context.Client.Where(i => i.LName == txt_LNameClient.Text).ToList().FirstOrDefault();
-                var Employ = ClassesHelper.AppData.context.Client.Where(i => i.LName == txt_LNamePersonal.Text).ToList().FirstOrDefault();
                 DateTime date = DateTime.Now;
-                Order order = new Order()
-                {
-                    IDEmploee = Employ.ID,
-                    IDClient = Client.ID,
-                    Start = date,
-                    TheEnd = date.AddMinutes(20)
-                };
-                ClassesHelper.AppData.context.Order.Add(order);
+
+                EF.Order addorder = new EF.Order();
+                addorder.IDClient = cmb_LNameClient.SelectedIndex + 1;
+                addorder.IDEmploee = cmb_LNamePersonal.SelectedIndex + 1;
+                addorder.IDService = cmb_TitleName.SelectedIndex + 1;
+                addorder.Start = date;
+                addorder.TheEnd = date.AddHours(1);
+
+                ClassesHelper.AppData.context.Order.Add(addorder);
                 ClassesHelper.AppData.context.SaveChanges();
-                MessageBox.Show("Заказ добавлен");
+
+                MessageBox.Show("Заказ успешно добавлен", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                this.Close();
             }
 
         }
